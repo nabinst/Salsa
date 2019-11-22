13c20db4ef52813d1e5346b1865f7fe6afd374f7
nabinst <nabinst@hotmail.com>
2019-11-22 15:32:50 -0500

    first commit

---
+++ static_in_env/scripts/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name everestSalsaLlcApp
+ * @description
+ * # everestSalsaLlcApp
+ *
+ * Main module of the application.
+ */
+/*
+angular
+  .module('everestSalsaLlcApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
+*/
+
+
+
+
+var everestSalsaLlcApp = angular.module('everestSalsaLlcApp', []);
---
+++ static_in_env/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name everestSalsaLlcApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the everestSalsaLlcApp
+ */
+angular.module('everestSalsaLlcApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
